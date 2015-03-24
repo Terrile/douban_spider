3be0118945ceac5713603b69c9993f0d98bc80a9
Terrile <minzou@live.com>
2015-03-24 22:40:26 +0800

    update start up scripts to limit the request in memory

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 mkdir jobcache
 scrapy crawl bookspider -s JOBDIR=jobcache --logfile=bookspider.log
 #update the JOBDIR para according to http://stackoverflow.com/questions/27943970/how-to-limit-scrapy-request-objects
-#according the refered url, the main purpose of jobdir is to limit the request # reside in memory
+#according the refered url, the main purpose of jobdir is to limit the request num reside in memory
